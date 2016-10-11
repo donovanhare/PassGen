a8861d901b0d8946057b890a0f44708db2f5dd57
Donovan <me.bigd@gmail.com>
2016-10-11 13:17:13 +0100

    Favicon in code

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.5.12/clipboard.min.js"></script>
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+	<link rel="shortcut icon" href="favicon.ico" />
 	<style type="text/css">
 		* {
 			font-family: 'Roboto Slab', serif;
